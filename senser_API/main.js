b63a0e6878abd1f5fc813382c23679f0d74f8696
xryuseix <ryusei143.shootingstar@gmail.com>
2021-07-23 23:57:54 +0900

    feat: APIを作成

--- senser_API/main.js
+++ senser_API/main.js
@@ -1,3 +1,19 @@
-function myFunction() {
-  
+/** @format */
+
+//HTTP GETをハンドリングする
+function doGet(e) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("latest");
+  var range = sheet.getRange("A1");
+  var value = range.getValue().split(",");
+
+  let json = {
+    date: value[0], //日付
+    temp: value[1], //温度
+    RH: value[2], //湿度
+    lumin: value[3], //照度
+  };
+
+  return ContentService.createTextOutput()
+    .setMimeType(ContentService.MimeType.JSON)
+    .setContent(JSON.stringify(json));
 }
