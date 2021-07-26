dc723ce4c85f8e6487e79f096d3f2080de339a10
xryuseix <ryusei143.shootingstar@gmail.com>
2021-07-27 02:24:51 +0900

    feat: ビデオで保存するようにした

--- camera.py
+++ camera.py
@@ -9,8 +9,18 @@ class SurveillanceCamera:
         self.__threshold = 10  # 閾値
         # カメラの開始
         self.__cam = cv2.VideoCapture(0)
+        self.__recording = False  # 録画中かどうか
+        self.__fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+        self.__camera_width = int(self.__cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.__camera_height = int(self.__cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.__camera_fps = 30
+        print("Setup finished!")
 
     def __del__(self):
+        # 録画の終了
+        if self.__recording:
+            self.__recording = False
+            self.__video.release()
         # カメラの終了
         self.__cam.release()
         cv2.destroyAllWindows()
@@ -26,13 +36,34 @@ class SurveillanceCamera:
             diff = self.__check_image(img1, img2, img3)
             # 差分がthの値以上なら動きがあったと判定
             cnt = cv2.countNonZero(diff)
-            if cnt > self.__threshold:
-                print("Human detected!")
+            if cnt > self.__threshold or self.__recording:
                 cv2.imshow("PUSH ENTER KEY", img3)
-                # 写真を画像
+
                 dt_now = datetime.datetime.now()
                 filename = f"{self.__save_path}/{dt_now.year}-{dt_now.month:02}-{dt_now.day:02}_{dt_now.hour:02}:{dt_now.minute:02}:{dt_now.second:06}_{dt_now.microsecond}"
-                cv2.imwrite(f"{filename}.jpg", img3)
+
+                # 録画開始
+                if not self.__recording:
+                    print("Human detected!")
+                    self.__recording = True
+                    self.__video = cv2.VideoWriter(
+                        f"{filename}.mp4",
+                        self.__fourcc,
+                        self.__camera_fps,
+                        (self.__camera_width, self.__camera_height),
+                    )
+                    self.__recording_start_time = dt_now
+
+                print("Recording...")
+                self.__video.write(img3)
+
+                # 録画終了
+                if (dt_now - self.__recording_start_time).seconds > 5:
+                    self.__recording = False
+                    self.__video.release()
+                    print("Record is finished!")
+
+                # cv2.imwrite(f"{filename}.jpg", img3)
             else:
                 cv2.imshow("PUSH ENTER KEY", diff)
             # 比較用の画像を保存
@@ -57,6 +88,10 @@ class SurveillanceCamera:
 
     # カメラから画像を取得する
     def __get_image(self):
-        img = self.__cam.read()[1]
-        img = cv2.resize(img, (600, 400))
+        _, img = self.__cam.read()
         return img
+
+
+if __name__ == "__main__":
+    camera = SurveillanceCamera()
+    camera.sensor()
